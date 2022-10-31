cebd25f4cbf60c08103342c40694c5d44cf4d1bf
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-31 16:28:05 -0700

    fixed call back/promise problem with recalling init fn

--- index.js
+++ index.js
@@ -39,6 +39,41 @@ const connection = mysql.createConnection({
     console.log('Connected to payroll_db ')
 );
 
+//originally wanted to get current payroll info for validation reasons;
+// let depts = [];
+// let roles = [];
+// let employees = [];
+// let mngrs = [];
+
+// connection.query('SELECT * FROM department', (err, res) => {
+//     err ? console.error(err) : res.forEach((e) => {
+//         depts.push(e);
+//     });
+// });
+// // console.log(depts);
+
+// connection.query('SELECT * FROM roles', (err, res) => {
+//     err ? console.error(err) : res.forEach((e) => {
+//         roles.push(e);
+//     });
+// });
+// // console.log(roles);
+
+
+// connection.query('SELECT * FROM employee WHERE manager_id IS NOT NULL', (err, res) => {
+//     err ? console.error(err) : res.forEach((e) => {
+//         employees.push(e);
+//     });
+// });
+// // console.log(employees);
+
+// connection.query('SELECT * FROM employee ', (err, res) => {
+//     err ? console.error(err) : res.forEach((e) => {
+//         mngrs.push(e);
+//     });
+// });
+// console.log(mngrs);
+
 function init() {
     inquirer
         .prompt([
@@ -48,62 +83,103 @@ function init() {
                 choices: choices,
                 name: "chosen",
             },
+            // {
+            //     type: "input",
+            //     message: "What do you want to call the new department?",
+            //     name: "deptName",
+            //     when: (answers) => {
+            //         if (answers.chosen === choices[3]) {
+            //             return true;
+            //         }
+            //     },
+            //     validate(answer) {
+            //         if (!answer) {
+            //             return "Your Department name cannot be blank"
+            //         }
+            //         return true
+            //     }
+            // },
+            // {
+            //     type: "input",
+            //     message: "What do you want to call the new position",
+            //     name: "roleTitle",
+            //     when: (answers) => {
+            //         if (answers.chosen === choices[4]) {
+            //             return true;
+            //         }
+            //     },
+            //     validate(answer) {
+            //         if (!answer) {
+            //             return "Your new role's name cannot be blank"
+            //         }
+            //         return true
+            //     }
+            // },
+            // {
+            //     type: "input",
+            //     message: "What would be the salary for this new role in USD?",
+            //     name: "roleUSD",
+            //     when: (answers) => {
+            //         if (answers.chosen === choices[4]) {
+            //             return true;
+            //         }
+            //     },
+            //     validate(answer) {
+            //         if (!answer || (isNaN(answer))) {
+            //             return "Your Department name cannot be blank && a valid number"
+            //         }
+            //         return true
+            //     }
+            // },
         ]).then((answers) => {
             console.log(answers.chosen);
             // console.log(choices[10]);
             const chosen = answers.chosen;
-
-            //added a end program option
+            console.log(answers.deptName);
+            // inquirer.prompt.ui.close();
+            // return ("test1");
+            // added a end program option
             if (chosen === choices[10]) {
                 console.log("Thank you and visit again", '\n', 'Use Ctrl-C to end the program');
                 return;
             } else if (chosen !== choices[10]) {
-                selectedOption(answers);
-            }
+                selectedOption(connection, answers);
+            };
         });
 };
 
 //swithc statement that depends on chosen option in init
-function selectedOption(answers) {
+function selectedOption(connection, answers) {
     switch (answers.chosen) {
         case choices[0]:
             viewAllDept(connection);
-            init();
             break;
         case choices[1]:
             viewAllRole(connection);
-            init();
             break;
         case choices[2]:
             viewAllEmp(connection);
-            init();
             break;
         case choices[3]:
-            addDept(connection);
-            init();
+            addDept(connection, init);
             break;
         case choices[4]:
-            init();
+            addRole(connection);
             break;
         case choices[5]:
             addEmp(connection, answers);
-            init();
             break;
         case choices[6]:
             updateEmpRole(connection, answers);
-            init();
             break;
         case choices[7]:
             updateEmpMngr(connection, answers);
-            init();
             break;
         case choices[8]:
             viewEmpByDept(connection, answers);
-            init();
             break;
         case choices[9]:
             viewEmpByMngr(connection, answers);
-            init();
             break;
     };
 };
