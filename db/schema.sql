392c7b8efaa04ac2e4a4b9e26a5d9041a96f975a
DraconMarius <113488982+DraconMarius@users.noreply.github.com>
2022-10-30 19:17:55 -0700

    added schema and seeds, double checked with  SELECT / DESCRIBE commands

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
-DROP DATABASE IF EXISTS payroll_db; --makesure there are no other databases first
-CREATE DATABASE payroll_db; --creating new payroll_db;
+DROP DATABASE IF EXISTS payroll_db; -- makesure there are no other databases first
+CREATE DATABASE payroll_db; -- creating new payroll_db;
 
---make sure to rememebr to 
+-- make sure to rememebr to 
 USE payroll_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL,
-    --setting department_id
+    -- setting department_id
     PRIMARY KEY(id)
 );
 
@@ -16,9 +16,9 @@ CREATE TABLE roles (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
-    --setting roles_id
-    PRIMARY KEY(id)
-    --getting the department's unique id
+    -- setting roles_id
+    PRIMARY KEY(id),
+    -- getting the department's unique id
     FOREIGN KEY(department_id)
     REFERENCES department(id)
 );
@@ -27,12 +27,12 @@ CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    roles_id INT,
     manager_id INT,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
     FOREIGN KEY(roles_id)
-    REFERENCES roles(id)
-    --another reference foreign key for manager id in the same table. Not 100% if possible
+    REFERENCES roles(id),
+    -- another reference foreign key for manager id in the same table. Not 100% if possible
     FOREIGN KEY(manager_id)
     REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,22 @@ USE payroll_db;
 
 INSERT INTO department (name)
 VALUES ('Marketing'),
-('HR'),
-('Engineering'),
-('Production'),
-('DEI');
+       ('HR'),
+       ('Engineering'),
+       ('DEI');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Marketing Lead', 150000, 1),
+       ('Marketer', 100000, 1),
+       ('HR Lead', 150000, 2),
+       ('Coordinator', 100000, 2),
+       ('Engineering Lead', 150000, 3),
+       ('SWE', 100000, 3),
+       ('Diversity Lead', 150000, 4),
+       ('Diversity Advocate', 100000, 4);
+
+INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES ('Quentin', 'Coldwater', 1, NULL),
+       ('Eliot', 'Waugh', 2, 1),
+       ('Julia', 'Wicker', 3, NULL),
+       ('Margo', 'Hanson', 4, 2);
